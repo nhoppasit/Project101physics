1622e464009a2b6815f167a8fe7f48db6536ecc0
Oak-surachet007x <Oak13gameroid@gmail.com>
2024-02-20 21:48:45 +0700

    add Script button
    add script DMM with MMC address in Button 1,2

--- CodingLabpro/CodingLabpro/Form1.Designer.cs
+++ CodingLabpro/CodingLabpro/Form1.Designer.cs
@@ -28,36 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtDMMAddress = new System.Windows.Forms.Button();
+            this.txtMMC2Address = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // txtDMMAddress
             // 
-            this.button1.Location = new System.Drawing.Point(88, 55);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.txtDMMAddress.Location = new System.Drawing.Point(88, 55);
+            this.txtDMMAddress.Name = "txtDMMAddress";
+            this.txtDMMAddress.Size = new System.Drawing.Size(75, 23);
+            this.txtDMMAddress.TabIndex = 0;
+            this.txtDMMAddress.Text = "button1";
+            this.txtDMMAddress.UseVisualStyleBackColor = true;
+            this.txtDMMAddress.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // txtMMC2Address
             // 
-            this.button2.Location = new System.Drawing.Point(88, 110);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.txtMMC2Address.Location = new System.Drawing.Point(88, 110);
+            this.txtMMC2Address.Name = "txtMMC2Address";
+            this.txtMMC2Address.Size = new System.Drawing.Size(75, 23);
+            this.txtMMC2Address.TabIndex = 1;
+            this.txtMMC2Address.Text = "button2";
+            this.txtMMC2Address.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(673, 350);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMMC2Address);
+            this.Controls.Add(this.txtDMMAddress);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -66,8 +66,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button txtDMMAddress;
+        private System.Windows.Forms.Button txtMMC2Address;
     }
 }
 
